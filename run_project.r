9ec3673821355a2ea69e7283c927d030eacbe089
avoRatovo <ratovoandry1@gmail.com>
2025-07-28 11:40:42 +0100

    automated data preparation, indicator calculation, and reporting

--- run_project.r
+++ run_project.r
@@ -1,3 +1,17 @@
 # Load required libraries
 library(readxl)
 library(dplyr)
+library(rmarkdown)
+
+# Execute data preparation script
+source("scripts/01_data_preparation.R")
+
+# Execute indicator calculation script
+source("scripts/02_indicator_calculation.R")
+
+# Render the final R Markdown report
+rmarkdown::render(
+  input = "scripts/03_Reporting.Rmd",
+  output_file = "../Reporting.html",
+  quiet = TRUE
+)

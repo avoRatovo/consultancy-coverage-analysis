675d492bd0bdeeffa376a2fcb46e15d8583166d5
avoRatovo <ratovoandry1@gmail.com>
2025-07-28 09:55:40 +0100

    Removed non-country entities from indicators dataset based on 'Geographic area'

--- scripts/01_data_preparation.r
+++ scripts/01_data_preparation.r
@@ -20,3 +20,30 @@ pop_data_proj <- read_excel("data/01_rawdata/WPP2022_GEN_F01_DEMOGRAPHIC_INDICAT
 # (Note: When importing Excel files, extra NA rows are sometimes added at the end.
 # In this case, after reviewing the dataset, there is one NA row at the end that needs to be removed.)
 indicators <- indicators[!is.na(indicators$`Geographic area`), ]
+
+
+# Remove regional and non-country entities
+# Vector of non-country entities
+non_country_entities <- c(
+  "(SDGRC) Central Africa", "(SDGRC) Eastern Africa", "(SDGRC) North Africa", "(SDGRC) Southern Africa",
+  "(SDGRC) United Nations Economic Commission for Africa", "(SDGRC) West Africa", "Africa", "African Union",
+  "Americas", "Arab Maghreb Union (AMU)", "Arab States", "Asia and the Pacific", "Caribbean",
+  "Central Africa (African Union)", "Central America", "Central and Southern Asia", "Central Asia",
+  "Common Market for Eastern and Southern Africa (COMESA)", "Community of Sahel-Saharan States (CEN-SAD)",
+  "East African Community (EAC)", "East and Southern Africa", "East Asia and Pacific", "Eastern Africa",
+  "Eastern Africa (African Union)", "Eastern and South-Eastern Asia", "Eastern and Southern Africa", "Eastern Asia",
+  "Eastern Europe and Central Asia", "Eastern Mediterranean", "Economic Community of Central African States (ECCAS)",
+  "Economic Community of West African States (ECOWAS 2025)", "Economic Community of West African States (ECOWAS)",
+  "Europe", "Europe and Central Asia", "Intergovernmental Authority on Development (IGAD)", "Latin America and the Caribbean",
+  "Least Developed Countries (LDC)", "Middle Africa", "Middle East and North Africa", "North America",
+  "Northern Africa", "Northern Africa (African Union)", "Northern Africa and Western Asia", "Northern America",
+  "Oceania", "Oceania (exc. Australia and New Zealand)", "SDG regions - Global", "South-East Asia",
+  "South-eastern Asia", "South America", "South Asia", "Southern Africa", "Southern Africa (African Union)",
+  "Southern African Development Community (SADC)", "Southern Asia", "Sub-Saharan Africa",
+  "UNICEF Programme Regions - Global", "UNICEF reporting regions - Global", "West and Central Africa",
+  "Western Africa", "Western Africa (African Union)", "Western Asia", "Western Europe", "Western Pacific",
+  "World Bank (high income)", "World Bank (low income)", "World Bank (lower middle income)", "World Bank (upper middle income)"
+)
+
+indicators <- indicators %>%
+  filter(!`Geographic area` %in% non_country_entities & !is.na(`Geographic area`))

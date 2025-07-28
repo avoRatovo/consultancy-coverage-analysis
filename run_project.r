34eb1a4a443f6efb60822a081235f454a2b156fc
avoRatovo <ratovoandry1@gmail.com>
2025-07-28 12:57:31 +0100

    Moved data import step from 01_data_preparation.R to run_project.R

--- run_project.r
+++ run_project.r
@@ -1,6 +1,21 @@
 # Load environment and dependencies
 source("user_profile.R")
 
+# 1. Import datasets
+# a) ANC4 and SBA coverage data (2018–2022)
+indicators <- read_excel("data/01_rawdata/GLOBAL_DATAFLOW_2018-2022.xlsx")
+
+# b) Country classification (on-track / off-track)
+status_data <- read_excel("data/01_rawdata/On-track and off-track countries.xlsx")
+
+# c) Population projections (estimated births in 2022)
+# Import Estimates sheet
+pop_data_est <- read_excel("data/01_rawdata/WPP2022_GEN_F01_DEMOGRAPHIC_INDICATORS_COMPACT_REV1.xlsx", sheet = "Estimates")
+
+# Import Projections sheet
+pop_data_proj <- read_excel("data/01_rawdata/WPP2022_GEN_F01_DEMOGRAPHIC_INDICATORS_COMPACT_REV1.xlsx", sheet = "Projections")
+
+
 # Execute data preparation script
 source("scripts/01_data_preparation.R")
 
